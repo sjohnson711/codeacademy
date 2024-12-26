86696963713ffd70f0c09c48994ff7ecfe4c2ef2
Seth Johnson <samaritanbrotherseth@gmail.com>
2024-12-26 18:32:40 +0000

    "at 5:29 in the video Asmr programming coding a snake game from 3 yrs ago - no talking"

--- snake.py
+++ snake.py
@@ -16,4 +16,43 @@ if(check_errors[1] > 0):
 else:
     print("Game Successfully initialized")
 
-    
+
+#intialise game windows
+
+
+pygame.display.set_caption("Snake Game")
+game_window = pygmae.display.set_mode(frame_size_x, frame_size_y)
+
+# colors
+black = pygame.Color(0,0,0)
+white = pygame.Color(255,255,255)
+red = pygame.Color(255,0,0)
+green = pygame.Color(0,255,0)
+blue = pygame.Color(0,0,255)
+
+
+fps_controller = pygame.time.Clock()
+# one snake square size
+square_size = 20 
+
+def init_vars():
+    global head_pos, snake_body, food_pos, food_spawn, score, direction
+    direction = "RIGHT"
+    head_pos = [120,60]
+    snake_body = [[120,60]]
+    food_pos = [random.randrange(1, (frame_size_x // square_size)) * square_size, 
+                random.randrange(1, (frame_size_y // square_size)) * square_size]
+
+    food_spawn = True
+    score = 0
+
+init_vars()
+# pause at 5:29 ASMR Programming - Coding a Snake Game - No talking
+def show_score():
+
+
+
+
+
+
+
