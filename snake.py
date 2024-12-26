26ae1eeb3e780446c7c1ec0c4ae8ea429535ddbc
Seth Johnson <samaritanbrotherseth@gmail.com>
2024-12-26 22:35:34 +0000

    "snake game update"

--- snake.py
+++ snake.py
@@ -21,7 +21,7 @@ else:
 
 
 pygame.display.set_caption("Snake Game")
-game_window = pygmae.display.set_mode(frame_size_x, frame_size_y)
+game_window = pygame.display.set_mode(frame_size_x, frame_size_y)
 
 # colors
 black = pygame.Color(0,0,0)
@@ -56,11 +56,66 @@ def show_score():
 
 while True:
     for event in pygame.event.get():
-        if event.type = pygame.QUIT:
-            pygame_quit
+        if event.type == pygame.QUIT:
+            pygame_quit()
+            sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            if ( event.key == pygame.K_UP or event.key == ord("w") 
+                and direction != "DOWN"):
+                direction = "UP"
+            elif ( event.key == pygame.K_DOWN or event.key == ord("s")
+                and direction != "UP"):
+                direction = "DOWN"
+            elif ( event.key == pygame.K_LEFT or event.key == ord("w")
+                and direction != "RIGHT"):
+                direction = "LEFT"
+            elif ( event.key == pygame.K_RIGHT or event.key == ord("w")
+                and direction != "LEFT"):
+                direction = "RIGHT"
 
 
+    if direction == "UP":
+        head_pos[1] -= square_size
+    elif direction == "DOWN":
+        head_pos[1] += square_size
+    elif direction == "LEFT":
+        head_pos[0] -= square_size
+    elif direction == "RIGHT":
+        head_pos[0] += square_size
 
+
+    if head_pos[0] < 0:
+        head_pos[0] = frame_size_x - square_size
+    elif head_pos[0] > frame_size_x - square_size
+        head_pos[0] = 0
+    elif head_pos[1] < 0:
+        head_pos[1] = frame_size_y - square_size
+    elif head_pos[1] > frame_size_y - square_size:
+        head_pos[1] = 0
+
+
+    #eating apple
+    snake_body.insert(0, list(head_pos))
+    if head_pos[0] == food_pos[0]and head_pos[1] == food_pos[1]:
+        score += 1
+        food_spawn = False
+    else:
+        snake_body.pop()
+
+    # spawn food
+    if not food_spawn:
+         food_pos = [random.randrange(1, (frame_size_x // square_size)) * square_size, 
+                random.randrange(1, (frame_size_y // square_size)) * square_size]
+
+# GFX
+game_window.fill(black)
+for pos in snake_body:
+    pygame.draw.rect(game_window, green, pygame.Rect(
+        pos[0] + 2, pos[1] + 2, 
+        square_size -2, square_size))
+
+pygame.draw.rect(game_window,red, pygame.Rect(food_pos[0]
+        ))
 
 
 
