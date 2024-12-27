b5a9797895fa345f5c22ea711361ea485acc74a2
Seth Johnson <samaritanbrotherseth@gmail.com>
2024-12-27 17:17:25 -0500

    Update Inheritance.py

--- Inheritance.py
+++ Inheritance.py
@@ -1,1 +1,55 @@
+class Person:
+  def __init__(self, name, age):
+      self.name = name
+      self.age = age
+  def print_info(self):
+      print(self.name)
+      print(self.age)
 
+class Teacher(Person):
+  def __init__(self, name, age, subject):
+      self.subject = subject
+
+      Person.__init__(self, name, age)
+
+
+myTeacher = Teacher("Dr. Hirani", 49, "Computer Science")
+myTeacher.print_info()
+print(myTeacher.subject)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+`
+
+class Dog:
+  def __init__(self, breed, age):
+    self.breed = breed
+    self.age = age
+
+  def my_dog(self):
+    print(f'Isha is a {self.breed}, and she is only {self.age}')
+
+pup = Dog('golden doodle', '2 years old)
+pup.my_dog()
